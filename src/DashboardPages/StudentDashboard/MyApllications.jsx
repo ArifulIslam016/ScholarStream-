130b950b5c6b6f39633b984f6b56c061b69070cb
Ariful Islam <arifulq234@gmail.com>
2025-12-16 01:23:58 +0600

    Details modal added on my application section

--- src/DashboardPages/StudentDashboard/MyApllications.jsx
+++ src/DashboardPages/StudentDashboard/MyApllications.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import useAuthhooks from "../../hooks/Authhooks";
 import { useQuery } from "@tanstack/react-query";
 import useSecureInstance from "../../hooks/SecureInstance";
@@ -9,9 +9,13 @@ import { MdDelete, MdRateReview } from "react-icons/md";
 import usePaymentHooks from "../../hooks/PaymentHooks";
 
 const MyApllications = () => {
+  const detailsModalRef = useRef();
+  const editModalRef = useRef();
   const { user } = useAuthhooks();
+  const [detailsModal, setDetailsModal] = useState({});
+  const [editsModal, seteEdiMsodal] = useState({});
   const Instance = useSecureInstance();
-  const handlePayments=usePaymentHooks()
+  const handlePayments = usePaymentHooks();
   const { data: applicationsData, isLoading } = useQuery({
     queryKey: ["applications", user],
     queryFn: async () => {
@@ -23,11 +27,21 @@ const MyApllications = () => {
   if (isLoading) {
     return <LoadingPage></LoadingPage>;
   }
-const handlePaymentFormMyApplicationPage=async(applicationdata)=>{
-const scholarshipInfo=await Instance.get(`/scholarship/${applicationdata.scholarshipId}`)
-handlePayments(scholarshipInfo.data)
-}
-  console.log(applicationsData);
+  const handlePaymentFormMyApplicationPage = async (applicationdata) => {
+    const scholarshipInfo = await Instance.get(
+      `/scholarship/${applicationdata.scholarshipId}`
+    );
+    handlePayments(scholarshipInfo.data);
+  };
+  const handleDetailsModal = (applicationData) => {
+    setDetailsModal(applicationData);
+    detailsModalRef.current.showModal();
+  };
+  const handleEditModal = (applicationData) => {
+    seteEdiMsodal(applicationData);
+    editModalRef.current.showModal();
+  };
+  console.log(detailsModal);
   return (
     <div>
       <h1 className="text-xl title font-medium">
@@ -77,6 +91,7 @@ handlePayments(scholarshipInfo.data)
                   </td>
                   <td className="flex gap-1">
                     <button
+                      onClick={() => handleDetailsModal(data)}
                       className="btn hover:tooltip  tooltip-primary"
                       data-tip="Details"
                     >
@@ -84,6 +99,7 @@ handlePayments(scholarshipInfo.data)
                     </button>
                     {data.applicationStatus === "pending" && (
                       <button
+                        onClick={() => handleEditModal(data)}
                         className="btn hover:tooltip  tooltip-primary"
                         data-tip="Edit"
                       >
@@ -92,7 +108,12 @@ handlePayments(scholarshipInfo.data)
                     )}
                     {data.paymentStatus === "unpaid" &&
                       data.applicationStatus === "pending" && (
-                        <button onClick={()=>handlePaymentFormMyApplicationPage(data)} className="btn btn-primary hover:tooltip  tooltip-primary">
+                        <button
+                          onClick={() =>
+                            handlePaymentFormMyApplicationPage(data)
+                          }
+                          className="btn btn-primary hover:tooltip  tooltip-primary"
+                        >
                           Pay
                         </button>
                       )}
@@ -119,6 +140,84 @@ handlePayments(scholarshipInfo.data)
           </tbody>
         </table>
       </div>
+      {/* details modal */}
+      <dialog
+        ref={detailsModalRef}
+        className="modal modal-bottom sm:modal-middle"
+      >
+        <div className="modal-box">
+          <div className="p-4 space-y-2">
+            <h2 className="text-lg font-bold">
+              {detailsModal.scholarshipName}
+            </h2>
+
+            <div>
+              <strong>User Name:</strong> {detailsModal.userName}
+            </div>
+            <div>
+              <strong>Email:</strong> {detailsModal.userEmail}
+            </div>
+            <div>
+              <strong>University:</strong> {detailsModal.universityName}
+            </div>
+            <div>
+              <strong>Degree:</strong> {detailsModal.degrees}
+            </div>
+            <div>
+              <strong>Category:</strong> {detailsModal.scholarshipCategory}
+            </div>
+            <div>
+              <strong>Application Fees:</strong> ${detailsModal.applicationFees}
+            </div>
+            <div>
+              <strong>Service Charge:</strong> ${detailsModal.serviceCharge}
+            </div>
+            <div>
+              <strong>Application Status:</strong>{" "}
+              {detailsModal.applicationStatus}
+            </div>
+            <div>
+              <strong>Payment Status:</strong> {detailsModal.paymentStatus}
+            </div>
+            <div>
+              <strong>Applied On:</strong>{" "}
+              {detailsModal.applicationDate &&
+                new Date(detailsModal.applicationDate).toLocaleDateString()}
+            </div>
+            <div>
+              <strong>Transition ID:</strong> {detailsModal.transitionId}
+            </div>
+            <div>
+              <strong>Scholarship ID:</strong> {detailsModal.scholarshipId}
+            </div>
+            <div>
+              <strong>User ID:</strong> {detailsModal.userId}
+            </div>
+          </div>
+
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+      {/* Edit modal */}
+      <dialog ref={editModalRef} className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
