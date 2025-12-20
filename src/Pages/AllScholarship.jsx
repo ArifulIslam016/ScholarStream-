5e9e6fad07776102217555626dff8ae302f3676d
Ariful Islam <arifulq234@gmail.com>
2025-12-21 00:15:32 +0600

    Filter functionality added on all scholarship page

--- src/Pages/AllScholarship.jsx
+++ src/Pages/AllScholarship.jsx
@@ -4,20 +4,21 @@ import { useQuery } from "@tanstack/react-query";
 import ScholarshipCard from "../Components/ScholarshipCard/ScholarshipCard";
 import { motion } from "motion/react";
 import LoadingPage from "./LoadingPage/LoadingPage";
+import { CiFilter } from "react-icons/ci";
 
 const AllScholarship = () => {
   const Instance = useSecureInstance();
   const [searchKey,setSearchKey]=useState('')
-  const [filterKey,setFilterKey]=useState('')
-  const { data, isLoading } = useQuery({
-    queryKey: ["allScholarship",searchKey],
+  const [catagory,setCatagory]=useState('')
+  const { data, isLoading, } = useQuery({
+    queryKey: ["allScholarship",searchKey,catagory],
     queryFn: async () => {
-      const res = await Instance.get(`/scholarships?search=${searchKey}`);
+      const res = await Instance.get(`/scholarships?search=${searchKey}&catagory=${catagory}`);
       return res.data;
     },
   });
-  const handleFilter=(key)=>{
-setFilterKey(key)
+  const handleSetCatagory=(key)=>{
+    setCatagory(key)
   }
 //   if (isLoading) {
 //     return  <div className="text-center  flex justify-center items-center text-blue-400 font-extrabold">
@@ -27,14 +28,13 @@ setFilterKey(key)
 //       </h1> </div>;
 //   }
   //   console.log(data)
-  console.log(filterKey)
   return (
     <div className="my-10">
       <div className="flex justify-between">
         <h1 className="text-2xl pl-1 md:pl-3 font-bold text-[#1d695e] ">
           Total {data?.ScholarshipData?.length} Scholarships Found
         </h1>
-        <div>
+        <div className="flex flex-col md:flex-row justify-center gap-2">
           <label className="input">
             <svg
               className="h-[1em] opacity-50"
@@ -54,6 +54,47 @@ setFilterKey(key)
             </svg>
             <input defaultValue={searchKey} onChange={(e)=>setSearchKey(e.target.value)} type="search" required placeholder="Search" />
           </label>
+          <div className="dropdown dropdown-center">
+                   <button tabIndex={20} role="button" className="btn py-1 btn-outline">
+                      <CiFilter /> catagory
+                   </button>
+                   <ul
+                     tabIndex="-1"
+                     className="dropdown-content  menu bg-base-100 rounded-box z-999 w-52 p-2 space-y-2 shadow-sm"
+                   >
+                     <li>
+                       <button onClick={() => handleSetCatagory("")} className="btn">
+                         All
+                       </button>
+                     </li>
+                     <li>
+                       <button
+                         onClick={() => setCatagory("Self-fund")}
+                         className="btn"
+                       >
+                         Self-fund
+                       </button>
+                     </li>
+         
+                     <li>
+                       <button
+                         onClick={() => setCatagory("Partial")}
+                         className="btn "
+                       >
+                         Partial
+                       </button>
+                     </li>
+         
+                     <li>
+                       <button
+                         onClick={() => setCatagory("Full fund")}
+                         className="btn"
+                       >
+                         Admin
+                       </button>
+                     </li>
+                   </ul>
+                 </div>
         </div>
       </div>
       {isLoading&& <LoadingPage></LoadingPage>}
